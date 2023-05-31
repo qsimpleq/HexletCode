4a1a04a8a139bdf719c49534578ea1d499f46e6c
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-05-31 16:17:24 +0500

    add method HexletCode.form_for

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,11 @@ require_relative 'hexlet_code/version'
 # Form builder loader
 module HexletCode
   autoload :Tag, 'hexlet_code/tag'
+
+  def self.form_for(_id, options = {}, &block)
+    options[:action] = options[:url] || '#'
+    options[:method] ||= 'post'
+
+    HexletCode::Tag.build(:form, options.filter { |key| key != :url }, &block)
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class HexletCodeTest < Minitest::Test
+  User = Struct.new(:name, :job, keyword_init: true)
+  ERROR = "Wrong value: %s.\nExpected: %s"
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
+  end
+
+  # noinspection RubyDynamicConstAssignment
+  def test_form_for
+    user = User.new name: 'Rob', job: 'Driver'
+
+    expected = '<form action="#" method="post"></form>'
+    result = HexletCode.form_for(user) { |f| }
+    assert expected == result, format(ERROR, result, expected)
+
+    expected = '<form action="/users" method="post"></form>'
+    result = HexletCode.form_for(user, url: '/users') { |f| }
+    assert expected == result, format(ERROR, result, expected)
   end
 end
