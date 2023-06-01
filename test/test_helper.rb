89410d8f1b1826b89fbdacf6f748cc74d93159b3
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-01 14:23:10 +0500

    update test_form_for

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,15 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'hexlet_code'
 
 require 'minitest/autorun'
+require 'yaml'
+
+HTML_ERROR = "Wrong value:\n%s\nExpected:\n%s"
+def fixture_load(name)
+  File.read "#{File.dirname(__FILE__)}/fixtures/#{name}"
+end
+
+def html_assert_equal(expected, result = nil)
+  expected = expected.strip
+  result = yield if block_given?
+  assert_equal expected, result, format(HTML_ERROR, result, expected)
+end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,23 +3,57 @@
 require 'test_helper'
 
 class HexletCodeTest < Minitest::Test
-  User = Struct.new(:name, :job, keyword_init: true)
-  ERROR = "Wrong value: %s.\nExpected: %s"
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
 
-  def test_that_it_has_a_version_number
+  def setup
+    @user = User.new name: 'rob', job: 'hexlet', gender: 'm'
+    @html = YAML.safe_load fixture_load('form_for_html.yml')
+  end
+
+  def test_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  # noinspection RubyDynamicConstAssignment
-  def test_form_for
-    user = User.new name: 'Rob', job: 'Driver'
+  def test_form_for_empty
+    html_assert_equal '<form action="#" method="post"></form>', HexletCode.form_for(@user)
+    html_assert_equal '<form action="/users" method="post"></form>', HexletCode.form_for(@user, url: '/users')
+  end
 
-    expected = '<form action="#" method="post"></form>'
-    result = HexletCode.form_for(user) { |f| }
-    assert expected == result, format(ERROR, result, expected)
+  def test_form_for_input_basic
+    html_assert_equal(@html[1], HexletCode.form_for(@user) do |f|
+      f.input :name
+      f.input :job, as: :text
+    end)
 
-    expected = '<form action="/users" method="post"></form>'
-    result = HexletCode.form_for(user, url: '/users') { |f| }
-    assert expected == result, format(ERROR, result, expected)
+    html_assert_equal(@html[2]) do
+      HexletCode.form_for @user, url: '#' do |f|
+        f.input :name, class: 'user-input'
+        f.input :job
+      end
+    end
+  end
+
+  def test_form_for_input_extra
+    html_assert_equal(@html[3]) do
+      HexletCode.form_for @user do |f|
+        f.input :job, as: :text
+      end
+    end
+
+    html_assert_equal(@html[4]) do
+      HexletCode.form_for @user, url: '#' do |f|
+        f.input :job, as: :text, rows: 50, cols: 50
+      end
+    end
+  end
+
+  def test_form_for_input_raise
+    assert_raises(NoMethodError) do
+      HexletCode.form_for(@user, url: '/users') do |f|
+        f.input :name
+        f.input :job, as: :text
+        f.input :age
+      end
+    end
   end
 end
