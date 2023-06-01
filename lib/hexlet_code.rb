baedd0b5c47c8206376bd679bcc427df2e9e9b92
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-01 14:24:32 +0500

    add class FormFor

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,12 +4,13 @@ require_relative 'hexlet_code/version'
 
 # Form builder loader
 module HexletCode
+  INDENT = ' ' * 2
+
   autoload :Tag, 'hexlet_code/tag'
+  autoload :FormFor, 'hexlet_code/form_for'
 
-  def self.form_for(_id, options = {}, &block)
-    options[:action] = options[:url] || '#'
-    options[:method] ||= 'post'
-
-    HexletCode::Tag.build(:form, options.filter { |key| key != :url }, &block)
+  def self.form_for(data, options = {})
+    form = HexletCode::FormFor.new(data, options)
+    form.render { yield form if block_given? }
   end
 end
---
+++ lib/hexlet_code/form_for.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module HexletCode
+  # Build a form instance
+  class FormFor
+    attr_accessor :data, :options, :nodes
+
+    def initialize(data, options = {})
+      options[:action] = options[:url] || '#'
+      options[:method] ||= 'post'
+      @data = data
+      @options = options
+      @nodes = []
+    end
+
+    def render
+      HexletCode::Tag.build(:form, @options.reject { |key| key == :url }) do
+        if block_given?
+          yield self
+          "\n#{nodes.map { |node| "#{INDENT}#{node}\n" }.join}" if nodes.any?
+        end
+      end
+    end
+
+    def input(name, options = {})
+      options[:value] = @data.public_send name
+
+      options[:name] = name
+      tag = 'input'
+      if options[:as] == :text
+        tag = 'textarea'
+        options[:cols] ||= 20
+        options[:rows] ||= 40
+      end
+      options[:type] ||= 'text' if tag == 'input'
+
+      reject_keys = ->(key) { key == :as || options[:as] == :text && key == :value }
+      node = HexletCode::Tag.build(tag, options.reject { |key| reject_keys.call key }) do
+        options[:value] if options[:as] == :text
+      end
+
+      @nodes << node
+
+      node
+    end
+  end
+end
