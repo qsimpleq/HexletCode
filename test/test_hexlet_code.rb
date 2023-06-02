5e4db01604d3a28aaa05ca2f13044d265b6e4bc2
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-02 21:19:37 +0500

    test_hexlet_code refactoring

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -7,7 +7,7 @@ class HexletCodeTest < Minitest::Test
 
   def setup
     @user = User.new(name: 'rob', job: 'hexlet', gender: 'm')
-    @html = YAML.safe_load(fixture_load('form_for_html.yml'))
+    @form_for_html = YAML.safe_load(fixture_load('form_for_html.yml'))
   end
 
   def test_a_version_number
@@ -19,35 +19,35 @@ class HexletCodeTest < Minitest::Test
     html_assert_equal '<form action="/users" method="post"></form>', HexletCode.form_for(@user, url: '/users')
   end
 
-  def test_form_for_input_basic
-    html_assert_equal(@html[1], HexletCode.form_for(@user) do |f|
-      f.input :name
-      f.input :job, as: :text
-    end)
+  def test_form_for_input
+    html_assert_equal(@form_for_html['input']) { HexletCode.form_for(@user) { |f| f.input :name } }
 
-    html_assert_equal(@html[2]) do
-      HexletCode.form_for @user, url: '#' do |f|
-        f.input :name, class: 'user-input'
-        f.input :job
-      end
+    html_assert_equal(@form_for_html['input_add_class']) do
+      HexletCode.form_for(@user, url: '#') { |f| f.input :name, class: 'user-input' }
     end
   end
 
-  def test_form_for_input_extra
-    html_assert_equal(@html[3]) do
-      HexletCode.form_for @user do |f|
-        f.input :job, as: :text
-      end
-    end
-
-    html_assert_equal(@html[4]) do
+  def test_form_for_textarea
+    html_assert_equal(@form_for_html['textarea']) do
       HexletCode.form_for @user, url: '#' do |f|
         f.input :job, as: :text, rows: 50, cols: 50
       end
     end
+
+    html_assert_equal(@form_for_html['textarea_defaults']) do
+      HexletCode.form_for(@user) { |f| f.input :job, as: :text }
+    end
   end
 
-  def test_form_for_input_raise
+  def test_form_for_submit
+    [['submit', nil], %w[submit_defaults Wow]].each do |(key, value)|
+      html_assert_equal(@form_for_html[key]) do
+        HexletCode.form_for(@user) { |f| f.submit value }
+      end
+    end
+  end
+
+  def test_form_for_raise
     assert_raises(NoMethodError) do
       HexletCode.form_for(@user, url: '/users') do |f|
         f.input :name
@@ -57,16 +57,13 @@ class HexletCodeTest < Minitest::Test
     end
   end
 
-  def test_form_for_submit
+  def test_form_for_group
     user = User.new job: 'hexlet'
-
-    [[5, nil], [6, 'Wow']].each do |(idx, value)|
-      html_assert_equal(@html[idx]) do
-        HexletCode.form_for user do |f|
-          f.input :name
-          f.input :job
-          f.submit value
-        end
+    html_assert_equal(@form_for_html['group_input_textarea_submit']) do
+      HexletCode.form_for(user, url: '/users') do |f|
+        f.input :name
+        f.input :job, as: :text
+        f.submit 'Wow'
       end
     end
   end
