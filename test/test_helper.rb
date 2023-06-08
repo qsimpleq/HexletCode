50d600d4b92509a953e2b77bc6c30961137fe40e
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-08 23:31:41 +0500

    html_assert_equal with standard messages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,6 @@ require 'hexlet_code'
 require 'minitest/autorun'
 require 'yaml'
 
-HTML_ERROR = "Wrong value:\n%s\nExpected:\n%s"
 def fixture_load(name)
   File.read "#{File.dirname(__FILE__)}/fixtures/#{name}"
 end
@@ -14,5 +13,5 @@ end
 def html_assert_equal(expected, result = nil)
   expected = expected.strip
   result = yield if block_given? && result.nil?
-  assert_equal expected, result, format(HTML_ERROR, result, expected)
+  assert_equal expected, result
 end
