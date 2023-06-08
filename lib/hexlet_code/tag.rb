9845c0d69ed1dd222940818344de0c0a4beb0eb2
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-08 23:30:37 +0500

    Tag refactoring

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,25 +1,34 @@
 # frozen_string_literal: true
 
 module HexletCode
-  # Generate HTML tag string
+  # Generate HTML form_tags string
   module Tag
     SINGLE_TAGS = %w[br hr img input wbr].freeze
 
-    def self.build(tag, options = {})
+    def self.build(tag, attributes = {}, block = nil)
       tag = tag.to_s.downcase
-      attributes = build_attributes(options)
+      attributes_string = build_attributes(attributes)
+      result = "<#{tag}#{attributes_string}>"
 
-      result = "<#{tag}#{attributes}>"
-      result += "#{yield if block_given?}</#{tag}>" unless SINGLE_TAGS.include?(tag)
+      unless SINGLE_TAGS.include?(tag)
+        content = if block_given?
+                    yield
+                  elsif !block.nil? && block.instance_of?(Proc)
+                    block.call
+                  end
+        result += "#{content}</#{tag}>"
+      end
 
       result
     end
 
     def self.build_attributes(attributes = {})
-      attributes.keys.sort.each_with_object([]) do |key, acc|
+      attributes
+        .keys
+        .sort
+        .each_with_object([]) do |key, acc|
         acc << " #{key}=\"#{attributes[key]}\""
       end.join
     end
-
   end
 end
