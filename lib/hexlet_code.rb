3aebec38fdc6d7b8c38bb667fdbc6df16d98ef2c
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-14 13:23:25 +0500

    Move Form class to HexletCode module

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,15 +2,33 @@
 
 require_relative 'hexlet_code/version'
 
+require_relative 'hexlet_code/form_builder'
+require_relative 'hexlet_code/form_renderer'
+require_relative 'hexlet_code/form_tags/form'
+
 # Form builder loader
 module HexletCode
   INDENT = ' ' * 2
 
   autoload :Tag, 'hexlet_code/tag'
-  autoload :Form, 'hexlet_code/form'
 
   def self.form_for(data, options = {})
-    form = HexletCode::Form.new(data, options) { yield _1 if block_given? }
-    form.render
+    form = Form.new(data, options) { yield _1 if block_given? }
+
+    form.render_by(:html)
+  end
+
+  class Form
+    attr_reader :form
+
+    def initialize(data = Struct.new, attributes = {}, &)
+      @form = HexletCode::FormBuilder.new(data, attributes, &)
+    end
+
+    def render_by(type)
+      renderer = HexletCode::FormRenderer.new(@form)
+
+      renderer.public_send("to_#{type}")
+    end
   end
 end
