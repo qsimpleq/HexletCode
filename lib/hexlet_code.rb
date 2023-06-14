2c277cf935f909d0ce9d0e529c2e0dae0f33031f
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-14 13:43:45 +0500

    Remove Form class

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
-
 require_relative 'hexlet_code/form_builder'
 require_relative 'hexlet_code/form_renderer'
 require_relative 'hexlet_code/form_tags/form'
@@ -10,25 +9,10 @@ require_relative 'hexlet_code/form_tags/form'
 module HexletCode
   INDENT = ' ' * 2
 
-  autoload :Tag, 'hexlet_code/tag'
+  def self.form_for(data, options = {}, &)
+    form = HexletCode::FormBuilder.new(data, options, &)
+    renderer = HexletCode::FormRenderer.new(form)
 
-  def self.form_for(data, options = {})
-    form = Form.new(data, options) { yield _1 if block_given? }
-
-    form.render_by(:html)
-  end
-
-  class Form
-    attr_reader :form
-
-    def initialize(data = Struct.new, attributes = {}, &)
-      @form = HexletCode::FormBuilder.new(data, attributes, &)
-    end
-
-    def render_by(type)
-      renderer = HexletCode::FormRenderer.new(@form)
-
-      renderer.public_send("to_#{type}")
-    end
+    renderer.to_html
   end
 end
