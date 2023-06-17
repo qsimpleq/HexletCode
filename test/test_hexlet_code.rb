14a42d75d08a9282f87ae6fc58730e830e635a16
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-06-17 14:03:25 +0500

    HexletCodeTest refactoring

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -7,23 +7,16 @@ class HexletCodeTest < Minitest::Test
 
   def setup
     @user = User.new(name: 'rob', job: 'hexlet', gender: 'm')
-    @form_for_html = {}
-    @fixture_files = %w[
-      form_for-post-empty.html
-      form_for-post-users-empty.html
-      form_for-post-empty-input.html
-      form_for-post-empty-input_add_class.html
-      form_for-post-empty-textarea.html
-      form_for-post-empty-textarea_defaults.html
-      form_for-post-empty-submit.html
-      form_for-post-empty-submit_defaults.html
-      form_for-post-users-group_input_textarea_submit.html
-    ]
 
-    @fixture_files.each do |file|
-      key = file.match(/(.+)\.html?$/)[1]
-      @form_for_html[key] = fixture_load(file)
-    end
+    @test_form_for_empty = fixture_load('form_for-post-empty.html')
+    @test_form_for_empty_users = fixture_load('form_for-post-users-empty.html')
+    @test_form_for_input = fixture_load('form_for-post-empty-input.html')
+    @test_form_for_input_add_class = fixture_load('form_for-post-empty-input_add_class.html')
+    @test_form_for_textarea = fixture_load('form_for-post-empty-textarea.html')
+    @test_form_for_textarea_defaults = fixture_load('form_for-post-empty-textarea_defaults.html')
+    @test_form_for_submit = fixture_load('form_for-post-empty-submit.html')
+    @test_form_for_submit_defaults = fixture_load('form_for-post-empty-submit_defaults.html')
+    @test_form_for_group = fixture_load('form_for-post-users-group_input_textarea_submit.html')
   end
 
   def test_a_version_number
@@ -31,34 +24,36 @@ class HexletCodeTest < Minitest::Test
   end
 
   def test_form_for_empty
-    html_assert_equal @form_for_html['form_for-post-empty'], HexletCode.form_for(@user)
-    html_assert_equal @form_for_html['form_for-post-users-empty'], HexletCode.form_for(@user, url: '/users')
+    html_assert_equal @test_form_for_empty, HexletCode.form_for(@user)
+    html_assert_equal @test_form_for_empty_users, HexletCode.form_for(@user, url: '/users')
   end
 
   def test_form_for_input
-    html_assert_equal(@form_for_html['form_for-post-empty-input']) { HexletCode.form_for(@user) { |f| f.input :name } }
+    html_assert_equal(@test_form_for_input) { HexletCode.form_for(@user) { |f| f.input :name } }
 
-    html_assert_equal(@form_for_html['form_for-post-empty-input_add_class']) do
+    html_assert_equal(@test_form_for_input_add_class) do
       HexletCode.form_for(@user, url: '#') { |f| f.input :name, class: 'user-input' }
     end
   end
 
   def test_form_for_textarea
-    html_assert_equal(@form_for_html['form_for-post-empty-textarea']) do
+    html_assert_equal(@test_form_for_textarea) do
       HexletCode.form_for @user, url: '#' do |f|
         f.input :job, as: :text, rows: 50, cols: 50
       end
     end
 
-    html_assert_equal(@form_for_html['form_for-post-empty-textarea_defaults']) do
+    html_assert_equal(@test_form_for_textarea_defaults) do
       HexletCode.form_for(@user) { |f| f.input :job, as: :text }
     end
   end
 
   def test_form_for_submit
-    [['form_for-post-empty-submit_defaults', nil],
-     %w[form_for-post-empty-submit Wow]].each do |(key, value)|
-      html_assert_equal(@form_for_html[key]) do
+    [
+      [@test_form_for_submit, 'Wow'],
+      [@test_form_for_submit_defaults, nil]
+    ].each do |(key, value)|
+      html_assert_equal(key) do
         HexletCode.form_for(@user) { |f| f.submit value }
       end
     end
@@ -76,7 +71,7 @@ class HexletCodeTest < Minitest::Test
 
   def test_form_for_group
     user = User.new job: 'hexlet'
-    html_assert_equal(@form_for_html['form_for-post-users-group_input_textarea_submit']) do
+    html_assert_equal(@test_form_for_group) do
       HexletCode.form_for(user, url: '/users') do |f|
         f.input :name
         f.input :job, as: :text
